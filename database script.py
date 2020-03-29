f4e043c3af47b4ca8a80a4e85be095c7ecc8cbb1
Franky Jiang <franky.jiang@ryerson.ca>
2020-03-29 00:23:40 -0400

    database script

--- database script.py
+++ database script.py
@@ -1,14 +1,21 @@
+#For situations where excel CVS format doesn't work
 import xlrd 
   
 # Give the location of the file 
-loc = (r'C:\Users\Franky Jiang\Documents\Workspace\AndroidChallenge.xlsx') 
+loc = (r'YOUR EXCEL FILE PATH HERE') 
   
 wb = xlrd.open_workbook(loc) 
 sheet = wb.sheet_by_index(0) 
-sheet.cell_value(0, 0) 
+sheet.cell_value(0, 0)
+#Please be sure that all values entered are text
 for i in range(1,sheet.nrows):
     a=[]
     for k in range(sheet.ncols):
         a += [sheet.cell_value(i, k)]
     a = tuple(a)
-    print("INSERT INTO `labs` (`id`, `Title`, `latitude`, `longitude`, `address`, `city`, `country`, `created_at`, `updated_at`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', NULL, NULL);" % a) 
+    #Populate fields
+    #Separate each column like so
+    #(`column1` ,`column2`, etc.....)
+    #include %s up to the number of columns you have
+    #you may also insert default values for certain columns
+    print("INSERT INTO `YOUR_DATABASE_NAME` (`POPULATE WITH COLUMNS HERE`) VALUES ('%s', '%s');" % a) 
